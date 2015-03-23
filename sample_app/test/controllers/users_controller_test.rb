bea5bfe55369bfa74c4b6eb838a709c97931cdbb
Art Beatte <art.beatte@docusign.com>
2015-03-23 13:07:17 -0700

    Refactors user new and edit layouts.

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -29,6 +29,15 @@ class UsersControllerTest < ActionController::TestCase
   		assert_redirected_to login_url
   	end
 
+    test "should not allow the admin attribute to be edited via the web" do
+      log_in_as(@other_user)
+      assert_not @other_user.admin?
+      patch :update, id: @other_user, user: { password:               @other_user.password, 
+                                              password_confirmation:  @other_user.password_confirmation,
+                                              admin: true }
+      assert_not @other_user.reload.admin?
+    end
+
   	test "should redirect edit when logged in as wrong user" do
   		log_in_as(@other_user)
   		get :edit, id: @user
